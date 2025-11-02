afc554b1a92d12c36d20ae5e7e637439b19dc4c5
kararAcode <63989704+kararAcode@users.noreply.github.com>
2025-11-02 15:08:30 -0600

    add endpoint to create invitations

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -186,6 +186,46 @@ app.post('/dummy-create-shift', async (req, res) => {
     }
 })
 
+app.post('/invitations', async (req, res) => {
+    const { userId } = getAuth(req)
+
+    const workspaceId = req.body.workspaceId
+
+    const user = await prisma.user.findFirst({
+        where: {
+            clerkId: userId,
+        },
+    })
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' })
+    }
+
+    const workspace = await prisma.workspace.findFirst({
+        where: {
+            id: workspaceId,
+        },
+    })
+
+    if (!workspace) {
+        return res.status(404).json({ error: 'User not found' })
+    }
+
+    if (workspace.adminId === user.id) {
+        const invitation = await prisma.invitation.create({
+            data: {
+                workspaceId,
+            },
+        })
+
+        res.status(200).json(invitation)
+    } else {
+        res.status(403).json({ error: 'Unauthorized to create invitations for the workspace' })
+    }
+});
+
+
+
 app.post('/clerk/webhook', async (req, res) => {
     const evt = await verifyWebhook(req)
     const { id } = evt.data
