bdf0776f98dc57e2506e7562bbe313ceed7f87df
Talha Hassan <qzl241@usask.ca>
2025-11-03 15:34:22 -0600

    fixed bug in navigation

--- apps/mobile/pages/LoginPage.tsx
+++ apps/mobile/pages/LoginPage.tsx
@@ -1,6 +1,11 @@
 import { View, Text, Button } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-export default function LoginPage({ navigation }) {
+type RootStackParamList = { Login: undefined; Home: undefined; };
+type LoginPageNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
+type Props = { navigation: LoginPageNavigationProp; };
+
+export default function LoginPage({ navigation }: Props) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Login Page</Text>
